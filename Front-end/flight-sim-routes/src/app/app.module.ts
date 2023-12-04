327731d1c9d0a749480224a47796902e6dea9803
Lucas Muller <lucas.mullerqdv@gmail.com>
2023-12-04 15:55:56 -0300

    Implemented sidebar Component

--- Front-end/flight-sim-routes/src/app/app.module.ts
+++ Front-end/flight-sim-routes/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { DonateComponent } from './donate/donate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
