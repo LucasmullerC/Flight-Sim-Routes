3eef64e8945fd10f6f298d7f12854df7e00bffe5
Lucas Muller <lucas.mullerqdv@gmail.com>
2023-12-04 17:17:56 -0300

    Implemented Donate Component and Active page

--- Front-end/flight-sim-routes/src/app/sidebar/sidebar.component.ts
+++ Front-end/flight-sim-routes/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+constructor(private router: Router) {}
+
+isRouteActive(route: string): boolean {
+  return this.router.isActive(route, true);
+}
 }
