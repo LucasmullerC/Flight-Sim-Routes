819da7d79a6e9db2cebc1c743953250e38ae653f
Lucas Muller <lucas.mullerqdv@gmail.com>
2023-12-19 12:33:32 -0300

    Updated airports-form styles

--- Front-end/flight-sim-routes/src/app/pages/schedules/pages/airports-form/airports-form.component.ts
+++ Front-end/flight-sim-routes/src/app/pages/schedules/pages/airports-form/airports-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ScheduleFormService } from 'src/app/services/schedule-form.service';
 
 @Component({
@@ -10,10 +11,20 @@ export class AirportsFormComponent {
   airlineName:string = '';
   columns: string[] = ['Airport ICAO'];
   rows: string[] = [];
+  demands: String[] = ['Extreme Demand',"Big Demand","Medium Demand","Low Demand"];
 
-  constructor(private scheduleForm: ScheduleFormService){}
+  constructor(private scheduleForm: ScheduleFormService,private formBuilder: FormBuilder){}
+
+  airportsForm: FormGroup = this.formBuilder.group({
+    icao:['', Validators.required],
+    demands:['', Validators.required],
+  });
 
   ngOnInit(): void {
     this.airlineName = this.scheduleForm.getAirlineName();
+  }
+
+  onFormControlChange(formControl: string, controlName: string) {
+    this.airportsForm.setControl(controlName, new FormControl(formControl));
   }
 }
